c944107406c919db81e69f082b2b02673760cd90
someh2705 <someh2705@gmail.com>
2021-12-07 00:57:00 +0900

    init: NavHost 설정 및 Hello World!

--- app/src/main/java/com/study/todo/MainActivity.kt
+++ app/src/main/java/com/study/todo/MainActivity.kt
@@ -3,13 +3,33 @@ package com.study.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.study.todo.ui.theme.TodoTheme
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+import javax.inject.Singleton
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +37,7 @@ class MainActivity : ComponentActivity() {
             TodoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
@@ -25,8 +45,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "example") {
+        composable("example") {
+            val exampleViewModel :ExampleViewModel = hiltViewModel()
+            ExampleScreen(exampleViewModel)
+        }
+    }
+}
+
 data class Example(
     val id: Int? = null,
     val message: String = "Hello World"
@@ -80,12 +108,28 @@ class ExampleViewModel @Inject constructor(
         }
     }
 }
+
+@Composable
+fun ExampleScreen(
+    exampleViewModel: ExampleViewModel = viewModel()
+) {
+    val data = exampleViewModel.example.observeAsState()
+
+    data.value?.let {
+        LazyColumn {
+            items(it) { example ->
+                Text(text = example.message)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    TodoTheme {
-        Greeting("Android")
+fun ExampleScreenPreview() {
+    MaterialTheme {
+        Surface {
+            ExampleScreen()
+        }
     }
 }
